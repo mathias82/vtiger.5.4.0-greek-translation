cc824632a90099ba23d89c2cd1dd87da6f7eb12e
mstavrou <m_stauroy@hotmail.com>
2014-06-28 15:18:01 +0300

    Update el_gr.lang.php

--- modules/Migration/language/el_gr.lang.php
+++ modules/Migration/language/el_gr.lang.php
@@ -15,10 +15,6 @@
 /*********************************************************************************
 GREEK TRANSLATION - LOCALISATION
 
-Prepared on February, 2013 (for vTigerCrm 5.4.0)
-
-Contributors:
-mathias stavrou m_stauroy@hotmail.com, Open For Business, http://www.mstavrou.com for HELLASCRM http://www.hellascrm.gr , Athens, Greece
  ********************************************************************************/
 $mod_strings = Array(
 'LBL_CRM_SETTINGS' => 'Ρυθμίσεις',
